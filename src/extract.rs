469cef332958460bfa3e6778c27d4a082a6b87cd
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-12-28 11:05:28 -0500

    make json extract errors more informative

--- src/extract.rs
+++ src/extract.rs
@@ -1,24 +1,16 @@
 use crate::Response;
 use axum::{
-    body::Bytes,
-    body::HttpBody,
+    body::{Bytes, HttpBody},
     extract::FromRequest,
     http::{header, Request},
 };
 use bytes::Buf;
-use essence::Error;
+use essence::error::{Error, MalformedBodyErrorType};
 use serde::de::DeserializeOwned;
 
 /// A JSON request body.
 #[derive(Clone, Debug)]
 pub struct Json<T>(pub T);
-
-#[inline]
-fn is_json_content_type<B>(req: &Request<B>) -> bool {
-    req.headers()
-        .get(header::CONTENT_TYPE)
-        .is_some_and(|content_type| content_type.to_str().is_ok_and(|s| s == "application/json"))
-}
 
 #[axum::async_trait]
 impl<T, B, S> FromRequest<S, B> for Json<T>
@@ -32,18 +24,48 @@ where
     type Rejection = Response<Error>;
 
     async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
-        let malformed = Response::from(Error::MalformedBody);
+        let content_type = req.headers().get(header::CONTENT_TYPE);
 
-        if !is_json_content_type(&req) {
-            return Err(malformed.clone());
+        if let Some(c) = content_type {
+            match c.to_str() {
+                Ok("application/json") => (),
+                Ok(c) => {
+                    return Err(Response::from(Error::MalformedBody {
+                        error_type: MalformedBodyErrorType::InvalidContentType,
+                        message: format!("expected content type application/json, got {c} instead"),
+                    }));
+                }
+                Err(_) => {
+                    return Err(Response::from(Error::MalformedBody {
+                        error_type: MalformedBodyErrorType::InvalidContentType,
+                        message: "expected content type application/json".to_string(),
+                    }));
+                }
+            }
+        } else {
+            return Err(Response::from(Error::MalformedBody {
+                error_type: MalformedBodyErrorType::InvalidContentType,
+                message: "expected content type header to be \"application/json\", but the header \
+                    was not present"
+                    .to_string(),
+            }));
         }
 
         // Deserialize the body using simd_json
         let body = Bytes::from_request(req, state)
             .await
-            .map_err(|_| malformed.clone())?;
+            .map_err(|err| Error::InternalError {
+                what: Some("deserialization"),
+                message: "failed to buffer request body".to_string(),
+                debug: Some(format!("{err:?}")),
+            })?;
+
         simd_json::from_reader(body.reader())
             .map(Json)
-            .map_err(|_| malformed)
+            .map_err(|err| Error::MalformedBody {
+                error_type: MalformedBodyErrorType::InvalidJson,
+                message: err.to_string(),
+            })
+            .map_err(Response::from)
     }
 }
