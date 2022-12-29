55b5183b3e4921c682e22e188e39535df42828b2
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-12-29 12:52:30 -0500

    add auth router in main router

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let router = Router::new()
         .route("/", get(|| async { (StatusCode::OK, "Hello from Adapt") }))
         .route("/teapot", get(|| async { StatusCode::IM_A_TEAPOT }))
+        .merge(routes::auth::router())
         .merge(routes::users::router());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8077));
