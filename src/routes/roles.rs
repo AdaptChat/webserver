02195aee613f3c742926cdb5c7f1acf45c7a0219
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-01-04 09:46:42 -0500

    add `edit_role` endpoint

--- src/routes/roles.rs
+++ src/routes/roles.rs
@@ -3,7 +3,7 @@ use crate::routes::NoContentResult;
 use crate::{extract::Auth, ratelimit::ratelimit, routes::RouteResult, Response};
 use axum::http::StatusCode;
 use axum::{extract::Path, handler::Handler, routing::get, Router};
-use essence::http::role::CreateRolePayload;
+use essence::http::role::{CreateRolePayload, EditRolePayload};
 use essence::models::{ModelType, Permissions};
 use essence::snowflake::generate_snowflake;
 use essence::Error;
@@ -87,6 +87,7 @@ pub async fn get_role(
 #[utoipa::path(
     post,
     path = "/guilds/{guild_id}/roles",
+    request_body = CreateRolePayload,
     responses(
         (status = CREATED, description = "Role object", body = Role),
         (status = UNAUTHORIZED, description = "Invalid token", body = Error),
@@ -129,6 +130,65 @@ pub async fn create_role(
 
     let role_id = generate_snowflake(ModelType::Role, 0); // TODO: node id
     let role = db.create_role(guild_id, role_id, payload).await?;
+    Ok(Response::ok(role))
+}
+
+/// Edit Role
+///
+/// Modifies the role with the given ID in the given guild. You must have the `MANAGE_ROLES`
+/// permission, and the role must be lower than your highest role.
+#[utoipa::path(
+    patch,
+    path = "/guilds/{guild_id}/roles/{role_id}",
+    request_body = EditRolePayload,
+    responses(
+        (status = OK, description = "Modified role object", body = Role),
+        (status = UNAUTHORIZED, description = "Invalid token", body = Error),
+        (
+            status = FORBIDDEN,
+            description = "\
+                You are forbidden from editing the role. This can be because:\n\
+                * You are not a member of the guild.\n\
+                * You do not have the `MANAGE_ROLES` permission in the guild.\n\
+                * You are editing a role that allows or denies permissions you do not have.\n\
+                * You are editing a role that is higher than your highest role.\n\
+            ",
+            body = Error,
+        ),
+        (status = NOT_FOUND, description = "Guild or role not found", body = Error),
+    ),
+    security(("token" = [])),
+)]
+pub async fn edit_role(
+    Auth(user_id, _): Auth,
+    Path((guild_id, role_id)): Path<(u64, u64)>,
+    Json(payload): Json<EditRolePayload>,
+) -> RouteResult<Role> {
+    if let Some(ref name) = payload.name {
+        validate_role_name(name)?;
+    }
+
+    let mut db = get_pool();
+    let member_permissions = db.fetch_member_permissions(guild_id, user_id, None).await?;
+    db.assert_member_has_permissions_with(guild_id, member_permissions, Permissions::MANAGE_ROLES)
+        .await?;
+    db.assert_top_role_higher_than(guild_id, user_id, role_id)
+        .await?;
+
+    if let Some(permissions) = payload.permissions {
+        if !permissions.allow.contains(member_permissions)
+            || !permissions.deny.contains(member_permissions)
+        {
+            return Err(Response::from(Error::MissingPermissions {
+                guild_id,
+                permissions: permissions.allow.difference(member_permissions)
+                    | permissions.deny.difference(member_permissions),
+                message: "You cannot edit a role to allow or deny permissions you do not have",
+            }));
+        }
+    }
+
+    let role = db.edit_role(guild_id, role_id, payload).await?;
     Ok(Response::ok(role))
 }
 
@@ -180,6 +240,8 @@ pub fn router() -> Router {
         )
         .route(
             "/guilds/:guild_id/roles/:role_id",
-            get(get_role.layer(ratelimit!(5, 8))).delete(delete_role.layer(ratelimit!(3, 10))),
+            get(get_role.layer(ratelimit!(5, 8)))
+                .patch(edit_role.layer(ratelimit!(3, 8)))
+                .delete(delete_role.layer(ratelimit!(3, 10))),
         )
 }
